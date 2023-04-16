db04587c74d3e3a880e58001d74cbe8ee02ae33d
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2023-04-16 23:36:39 +0800

    fix bug

--- src/main/kotlin/dao/Characters.kt
+++ src/main/kotlin/dao/Characters.kt
@@ -1,9 +1,10 @@
 ﻿package com.github.zzwtsy.dao
+
 import com.github.zzwtsy.models.Character
 import org.ktorm.schema.*
 
-object Characters : Table<Character>("characters"){
-	var id = varchar("id").primaryKey().bindTo { it.id }
-	var name = text("name").bindTo { it.name }
-	var strategyMd5 = text("strategy_md5").bindTo { it.strategyMd5 }
+object Characters : Table<Character>("Characters") {
+    var id = varchar("id").primaryKey().bindTo { it.id }
+    var name = text("name").bindTo { it.name }
+    var strategyMd5 = text("strategy_md5").bindTo { it.strategyMd5 }
 }
